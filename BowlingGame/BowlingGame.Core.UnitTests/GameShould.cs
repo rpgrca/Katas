822ece3a400d28251e0c76c61307d0ac62fa1142
Roberto <rpgrca@gmail.com>
2020-05-01 19:08:44 -0300

    handling spare at the 10th frame

--- BowlingGame/BowlingGame.Core.UnitTests/GameShould.cs
+++ BowlingGame/BowlingGame.Core.UnitTests/GameShould.cs
@@ -78,6 +78,15 @@ namespace BowlingGame.Core.UnitTests
             Assert.Contains(Game.INVALID_AMOUNT_OF_PINES_EXCEPTION, exception.Message);
         }
 
+        [Fact]
+        public void WhenGettingASpareButWithoutExtraRoll_ScoreShouldBe10()
+        {
+            var game = new Game();
+            game.Roll(5);
+            game.Roll(5);
+            Assert.Equal(10, game.Score());
+        }
+
         [Theory]
         [InlineData(5, 5, 5, 20)]
         [InlineData(1, 9, 9, 28)]
@@ -92,12 +101,130 @@ namespace BowlingGame.Core.UnitTests
         }
 
         [Fact]
-        public void WhenGettingASpareButWithoutExtraRoll_ScoreShouldBe10()
+        public void WhenLastFrameHasSpare_AnExtraRollShouldBeAwarded()
         {
             var game = new Game();
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(1);
+            game.Roll(9); // 10.2
             game.Roll(5);
-            game.Roll(5);
+            Assert.Equal(33, game.Score());
+        }
+
+        [Fact]
+        public void WhenGettingAnStrikeWithoutExtraRolls_ScoreShouldBe10()
+        {
+            var game = new Game();
+            game.Roll(10);
             Assert.Equal(10, game.Score());
+        }
+
+        [Fact]
+        public void WhenGettingAnStrike_NextRollShouldBeCountedTwice()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(1);
+            Assert.Equal(12, game.Score());
+        }
+
+        [Fact]
+        public void WhenGettingAnStrike_NextTwoRollsShouldBeCountedTwice()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(2);
+            game.Roll(2);
+            Assert.Equal(18, game.Score());
+        }
+
+        [Fact]
+        public void WhenGettingTwoConsecutiveStrikes_NextRollShouldBeCountedThrice()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(5);
+            Assert.Equal(40, game.Score());
+        }
+
+        [Fact]
+        public void WhenGettingTwoConsecutiveStrikes_ShouldAccountForFourBonuses()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(4);
+            game.Roll(5);
+            Assert.Equal(48, game.Score());
+        }
+
+        [Fact]
+        public void WhenRollingStrikes_FrameShouldEndEarlier()
+        {
+            var game = new Game();
+            game.Roll(10); // 1
+            game.Roll(0);  // 2.1
+            game.Roll(0);  // 2.2
+            game.Roll(10); // 3
+            game.Roll(0);  // 4.1
+            game.Roll(0);  // 4.2
+            game.Roll(10); // 5
+            game.Roll(0);  // 6.1
+            game.Roll(0);  // 6.2
+            game.Roll(10); // 7
+            game.Roll(0);  // 8.1
+            game.Roll(0);  // 8.2
+            game.Roll(10); // 9
+            game.Roll(0);  // 10.1
+            game.Roll(0);  // 10.2
+            var exception = Assert.Throws<ArgumentException>(() => game.Roll(1));
+            Assert.Contains(Game.INVALID_AMOUNT_OF_FRAMES_EXCEPTION, exception.Message);
+        }
+
+        [Fact]
+        public void WhenPlayingASampleBowlingGame_ExtraRollsShouldNotBeAllowed()
+        {
+            var game = new Game();
+            game.Roll(1);  // 1.1
+            game.Roll(4);  // 1.2
+            game.Roll(4);  // 2.1
+            game.Roll(5);  // 2.2
+            game.Roll(6);  // 3.1
+            game.Roll(4);  // 3.2
+            game.Roll(5);  // 4.1
+            game.Roll(5);  // 4.2
+            game.Roll(10); // 5
+            game.Roll(0);  // 6.1
+            game.Roll(1);  // 6.2
+            game.Roll(7);  // 7.1
+            game.Roll(3);  // 7.2
+            game.Roll(6);  // 8.1
+            game.Roll(4);  // 8.2
+            game.Roll(10); // 9
+            game.Roll(2);  // 10.1
+            game.Roll(8);  // 10.2
+            game.Roll(6);  // 11
+            var exception = Assert.Throws<ArgumentException>(() => game.Roll(1));
+            Assert.Equal(Game.INVALID_AMOUNT_OF_FRAMES_EXCEPTION, exception.Message);
+            //Assert.Equal(133, game.Score());
         }
     }
 }
--- BowlingGame/BowlingGame.Core/Game.cs
+++ BowlingGame/BowlingGame.Core/Game.cs
@@ -1,6 +1,169 @@
-namespace BowlingGame.Core.UnitTests
+using System;
+using System.Linq;
+
+namespace BowlingGame.Core
 {
-    public partial class Game
+    public class Game
     {
+        private readonly (int, int)[] _frames = new[]
+        {
+            (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1)
+        };
+
+        private int _extra_rolls = 0;
+        private int _index = 0;
+        public const string INVALID_AMOUNT_OF_FRAMES_EXCEPTION = "Invalid amount of frames";
+        public const string INVALID_AMOUNT_OF_PINES_EXCEPTION = "Invalid amount of pines";
+
+        public int Score()
+        {
+            var result = 0;
+            var bonus = 0;
+            
+            for (var i = 0; i <= _index; i++)
+            {
+                var frameScore = 0;
+                var (roll1, roll2) = _frames[i];
+                {
+                    if (roll1 != -1) // Always != -1
+                    {
+                        if (roll1 == 10 && _index < 10)
+                        {
+                            if (bonus > 0)
+                            {
+                                frameScore += roll1;
+                                bonus--;
+                            }
+
+                            bonus += 2;
+                        }
+                        else
+                        {
+                            if (bonus > 0)
+                            {
+                                frameScore += roll1;
+                                bonus--;
+                            }
+                        }
+ 
+                        frameScore += roll1;
+                    }
+
+                    if (roll2 != -1)
+                    {
+                        if (roll1 + roll2 == 10 && _index < 10)
+                        {
+                            bonus += 1;
+                        }
+                        else
+                        {
+                            if (bonus > 0)
+                            {
+                                frameScore += roll2;
+                                bonus--;
+                            }
+                        }
+                        
+                        frameScore += roll2;
+                    }
+                }
+               
+                result += frameScore;
+            }
+            
+            return result;
+        }
+
+        public void Roll(int pins)
+        {
+            if (pins > 10 || pins < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pins), INVALID_AMOUNT_OF_PINES_EXCEPTION);
+            }
+
+            if (_index < 10 && _frames[_index].Item1 == -1)
+            {
+                _frames[_index].Item1 = pins;
+                if (pins == 10)
+                {
+                    _index++;
+                }
+            }
+            else if (_index < 10 && _frames[_index].Item2 == -1)
+            {
+                _frames[_index].Item2 = pins;
+            }
+            else
+            {
+                if (++_index < 10)
+                {
+                    _frames[_index].Item1 = pins;
+                    if (pins == 10)
+                    {
+                        _index++;
+                    }
+                }
+                else
+                {
+                    if (_frames[9].Item1 == 10)
+                    {
+                        if (_frames[10].Item1 == 10)
+                        {
+                            if (_frames[11].Item1 == -1)
+                            {
+                                _frames[11].Item1 = pins;
+                            }
+                            else
+                            {
+                                throw new ArgumentException(INVALID_AMOUNT_OF_FRAMES_EXCEPTION);
+                            }
+                        }
+                        else
+                        {
+                            if (_frames[10].Item1 == -1)
+                            {
+                                _frames[10].Item1 = pins;
+                            }
+                            else
+                            {
+                                if (_frames[10].Item2 == -1)
+                                {
+                                    _frames[10].Item2 = pins;
+                                }
+                                else
+                                {
+                                    if (_frames[11].Item1 == -1)
+                                    {
+                                        _frames[10].Item1 = pins;
+                                    }
+                                    else
+                                    {
+                                        throw new ArgumentException(INVALID_AMOUNT_OF_FRAMES_EXCEPTION);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (_frames[9].Item1 + _frames[9].Item2 == 10)
+                        {
+                            if (_frames[10].Item1 == -1)
+                            {
+                                _frames[10].Item1 = pins;
+                            }
+                            else
+                            {
+                                throw new ArgumentException(INVALID_AMOUNT_OF_FRAMES_EXCEPTION);
+                            }
+                        }
+                        else
+                        {
+                            throw new ArgumentException(INVALID_AMOUNT_OF_FRAMES_EXCEPTION);
+                        }                       
+                    }
+                }
+            }
+        }
     }
 }
