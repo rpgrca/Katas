aa1bc555f690d1d6974a177f2076d56da943ecca
Roberto <rpgrca@gmail.com>
2020-05-04 01:32:01 -0300

    fixed bug with enum with value 0

--- TicTacToe/TicTacToe.Core.UnitTests/TicTacToeShould.cs
+++ TicTacToe/TicTacToe.Core.UnitTests/TicTacToeShould.cs
@@ -9,13 +9,13 @@ namespace TicTacToe.Core.UnitTests
         public void GivenANewTicTacToeGame_WhenStarting_ThenThereIsNoWinner()
         {
             var ticTacToe = new TicTacToe();
-            Assert.False(ticTacToe.HasWinner());
+            Assert.Equal(Result.Draw, ticTacToe.GetResult());
         }
 
         [Theory]
         [InlineData(Player.X, Player.O)]
         [InlineData(Player.O, Player.X)]
-        public void GivenANewTicTacToeGameStartingWithPlayer_WhenTheOtherTriesToMove_AnExceptionIsThrown(Player starting, Player playing)
+        public void GivenANewTicTacToeGameStartingWithPlayer_WhenTheOtherTriesToMove_ThenAnExceptionIsThrown(Player starting, Player playing)
         {
             var ticTacToe = new TicTacToe();
             ticTacToe.StartWith(starting);
@@ -23,15 +23,104 @@ namespace TicTacToe.Core.UnitTests
             Assert.Equal(TicTacToe.PLAYER_TURN_IS_INVALID_EXCEPTION, exception.Message);
         }
 
+        [Fact]
+        public void GivenANewTicTacToeStartingWithDefaultPlayer_WhenPlayerOTriesToMove_ThenAnExceptionIsThrown()
+        {
+            var ticTacToe = new TicTacToe();
+            var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(Player.O, 0, 0));
+            Assert.Equal(TicTacToe.PLAYER_TURN_IS_INVALID_EXCEPTION, exception.Message);
+        }
+
         [Theory]
         [InlineData(-1)]
         [InlineData(3)]
-        public void Test1(int invalidY)
+        public void GivenANewTicTacToeGame_WhenTryingToPlayAnInvalidYMovement_ThenAnExceptionIsThrown(int invalidY)
         {
             var ticTacToe = new TicTacToe();
-            ticTacToe.StartWith(Player.X);
             var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(Player.X, 0, invalidY));
             Assert.Equal(TicTacToe.PLAYER_MOVEMENT_IS_INVALID_EXCEPTION, exception.Message);
+        }
+        
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(3)]
+        public void GivenANewTicTacToeGame_WhenTryingToPlayAnInvalidXMovement_ThenAnExceptionIsThrown(int invalidX)
+         {
+             var ticTacToe = new TicTacToe();
+             var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(Player.X, invalidX, 0));
+             Assert.Equal(TicTacToe.PLAYER_MOVEMENT_IS_INVALID_EXCEPTION, exception.Message);
+         }
+
+        [Fact]
+        public void GivenATicTacToeGameWithAPlayedTurn_WhenSamePlayerTriesToPlayAgain_ThenAnExceptionIsThrown()
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.Play(Player.X, 0, 0);
+            var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(Player.X, 1, 0));
+            Assert.Equal(TicTacToe.PLAYER_TURN_IS_INVALID_EXCEPTION, exception.Message);
+        }
+
+        [Fact]
+        public void GivenATicTacToeGameWithPlayedTurn_WhenPlayerPlaysSameMovement_ThenAnExceptionIsThrown()
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.Play(Player.X, 0, 0);
+            var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(Player.O, 0, 0));
+            Assert.Equal(TicTacToe.PLAYER_MOVEMENT_IS_INVALID_EXCEPTION, exception.Message);
+        }
+
+        [Fact]
+        public void GivenATicTacToeGame_WhenPlayerHasFirstRow_ThenResultShowsWinner()
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.Play(Player.X, 0, 0);
+            ticTacToe.Play(Player.O, 1, 0);
+            ticTacToe.Play(Player.X, 0, 1);
+            ticTacToe.Play(Player.O, 2, 0);
+            ticTacToe.Play(Player.X, 0, 2);
+            Assert.Equal(Result.XWon, ticTacToe.GetResult());
+        }
+
+        [Fact]
+        public void GivenATicTacToeGameWithAWinner_WhenTheOtherPlayerPlays_ThenAnExceptionIsThrown()
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.Play(Player.X, 0, 0);
+            ticTacToe.Play(Player.O, 1, 0);
+            ticTacToe.Play(Player.X, 0, 1);
+            ticTacToe.Play(Player.O, 2, 0);
+            ticTacToe.Play(Player.X, 0, 2);
+            var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(Player.O, 2, 2));
+            Assert.Equal(TicTacToe.PLAYER_MOVEMENT_IS_INVALID_EXCEPTION, exception.Message);
+        }
+
+        [Fact]
+        public void GivenATicTacToeGame_WhenPlayerHasSecondRow_ThenResultShowsWinner()
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.Play(Player.X, 0, 0);
+            ticTacToe.Play(Player.O, 1, 0);
+            ticTacToe.Play(Player.X, 0, 1);
+            ticTacToe.Play(Player.O, 1, 1);
+            ticTacToe.Play(Player.X, 2, 2);
+            ticTacToe.Play(Player.O, 1, 2);
+            Assert.Equal(Result.OWon, ticTacToe.GetResult());
+        }
+
+        [Fact]
+        public void GivenATicTacToeGame_WhenBoardIsFilled_ThenGameEndsInDraw()
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.Play(Player.X, 0, 0);
+            ticTacToe.Play(Player.O, 0, 1);
+            ticTacToe.Play(Player.X, 0, 2);
+            ticTacToe.Play(Player.O, 1, 0);
+            ticTacToe.Play(Player.X, 1, 1);
+            ticTacToe.Play(Player.O, 2, 2);
+            ticTacToe.Play(Player.X, 2, 1);
+            ticTacToe.Play(Player.O, 2, 0);
+            ticTacToe.Play(Player.X, 1, 2);
+            Assert.Equal(Result.Draw, ticTacToe.GetResult());
         }
     }
 }
--- TicTacToe/TicTacToe.Core/TicTacToe.cs
+++ TicTacToe/TicTacToe.Core/TicTacToe.cs
@@ -1,23 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TicTacToe.Core
 {
     public enum Player
     {
-        X,
-        O
+        X = 1,
+        O = 2
     }
-     
+
+    public enum Result
+    {
+        Draw = 0,
+        XWon = Player.X,
+        OWon = Player.O,
+    }
+    
     public class TicTacToe
     {
+        private enum PlayerSquare
+        {
+            N = 0,
+            X = Player.X,
+            O = Player.O
+        }
+        
         public const string PLAYER_MOVEMENT_IS_INVALID_EXCEPTION = "The chosen coordinates are invalid.";
         public const string PLAYER_TURN_IS_INVALID_EXCEPTION = "Player turn is invalid.";
+        
         private Player _currentPlayer = Player.X;
+        private readonly PlayerSquare[] _board = { PlayerSquare.N, PlayerSquare.N, PlayerSquare.N,
+              PlayerSquare.N, PlayerSquare.N, PlayerSquare.N,
+              PlayerSquare.N, PlayerSquare.N, PlayerSquare.N };
 
-        public bool HasWinner()
+        private readonly List<int[]> _winningCombinations = new List<int[]>
         {
-            return false;
+            new [] { 0, 1, 2 }, new [] { 3, 4, 5 }, new [] { 6, 7, 8 },
+            new [] { 0, 3, 6 }, new [] { 1, 4, 7 }, new [] { 2, 5, 8 },
+            new [] { 0, 4, 8 }, new [] { 2, 4, 6 }
+        };
+
+        public Result GetResult()
+        {
+            foreach (var winningCombination in _winningCombinations)
+            {
+                if (_board[winningCombination[0]] == _board[winningCombination[1]] &&
+                    _board[winningCombination[1]] == _board[winningCombination[2]] &&
+                    _board[winningCombination[0]] != PlayerSquare.N)
+                {
+                    return PlayerToResult(_board[winningCombination[0]]);
+                }
+            }
+
+            return Result.Draw;
         }
+        /*
+        public Result GetResult() =>
+            _winningCombinations
+                .Where(p =>
+                    _board[p[0]] == _board[p[1]] &&
+                    _board[p[1]] == _board[p[2]] &&
+                    _board[p[0]] != PlayerSquare.N)
+                .Select(p => PlayerToResult(_board[p[0]]))
+                .Single();*/
+
+        private static Result PlayerToResult(PlayerSquare square) =>
+            square switch
+            {
+                PlayerSquare.X => Result.XWon,
+                PlayerSquare.O => Result.OWon,
+                _ => Result.Draw
+            };
 
         public void StartWith(Player startingPlayer) =>
             _currentPlayer = startingPlayer;
@@ -27,12 +81,30 @@ namespace TicTacToe.Core
             if (_currentPlayer != player)
             {
                 throw new ArgumentException(PLAYER_TURN_IS_INVALID_EXCEPTION);
+                
             }
 
-            if (y < 0 || y > 2)
+            var movement = y * 3 + x;
+            if (x < 0 || x > 2 || y < 0 || y > 2 || _board[movement] != PlayerSquare.N)
             {
                 throw new ArgumentException(PLAYER_MOVEMENT_IS_INVALID_EXCEPTION);
             }
+
+            if (!HasFreeSquares())
+            {
+                throw new ArgumentException(PLAYER_MOVEMENT_IS_INVALID_EXCEPTION);
+            }
+
+            if (GetResult() != Result.Draw)
+            {
+                throw new ArgumentException(PLAYER_MOVEMENT_IS_INVALID_EXCEPTION);
+            }
+
+            _board[movement] = (PlayerSquare)player;
+            _currentPlayer = _currentPlayer == Player.O ? Player.X : Player.O;
         }
+
+        private bool HasFreeSquares() =>
+            _board.Any(p => p == PlayerSquare.N);
     }
 }
