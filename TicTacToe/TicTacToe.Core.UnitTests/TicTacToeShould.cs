98bec24b8d4dd3b488d857f1270d1cad6d5e045a
Roberto <rpgrca@gmail.com>
2020-05-03 21:48:38 -0300

    add start player, check coordinates

--- TicTacToe/TicTacToe.Core.UnitTests/TicTacToeShould.cs
+++ TicTacToe/TicTacToe.Core.UnitTests/TicTacToeShould.cs
@@ -6,10 +6,32 @@ namespace TicTacToe.Core.UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GivenANewTicTacToeGame_WhenStarting_ThenThereIsNoWinner()
         {
             var ticTacToe = new TicTacToe();
             Assert.False(ticTacToe.HasWinner());
+        }
+
+        [Theory]
+        [InlineData(Player.X, Player.O)]
+        [InlineData(Player.O, Player.X)]
+        public void GivenANewTicTacToeGameStartingWithPlayer_WhenTheOtherTriesToMove_AnExceptionIsThrown(Player starting, Player playing)
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.StartWith(starting);
+            var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(playing, 0, 0));
+            Assert.Equal(TicTacToe.PLAYER_TURN_IS_INVALID_EXCEPTION, exception.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(3)]
+        public void Test1(int invalidY)
+        {
+            var ticTacToe = new TicTacToe();
+            ticTacToe.StartWith(Player.X);
+            var exception = Assert.Throws<ArgumentException>(() => ticTacToe.Play(Player.X, 0, invalidY));
+            Assert.Equal(TicTacToe.PLAYER_MOVEMENT_IS_INVALID_EXCEPTION, exception.Message);
         }
     }
 }
--- TicTacToe/TicTacToe.Core/TicTacToe.cs
+++ TicTacToe/TicTacToe.Core/TicTacToe.cs
@@ -2,11 +2,37 @@
 
 namespace TicTacToe.Core
 {
+    public enum Player
+    {
+        X,
+        O
+    }
+     
     public class TicTacToe
     {
+        public const string PLAYER_MOVEMENT_IS_INVALID_EXCEPTION = "The chosen coordinates are invalid.";
+        public const string PLAYER_TURN_IS_INVALID_EXCEPTION = "Player turn is invalid.";
+        private Player _currentPlayer = Player.X;
+
         public bool HasWinner()
         {
             return false;
+        }
+
+        public void StartWith(Player startingPlayer) =>
+            _currentPlayer = startingPlayer;
+
+        public void Play(Player player, int x, int y)
+        {
+            if (_currentPlayer != player)
+            {
+                throw new ArgumentException(PLAYER_TURN_IS_INVALID_EXCEPTION);
+            }
+
+            if (y < 0 || y > 2)
+            {
+                throw new ArgumentException(PLAYER_MOVEMENT_IS_INVALID_EXCEPTION);
+            }
         }
     }
 }
