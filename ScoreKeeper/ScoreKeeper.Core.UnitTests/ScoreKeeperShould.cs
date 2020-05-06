2e50e7c59f6a6277e81a9df5bcc229169ff15fa9
Roberto <rpgrca@gmail.com>
2020-05-06 10:27:58 -0300

    added constructor params, check for invalid values

--- ScoreKeeper/ScoreKeeper.Core.UnitTests/ScoreKeeperShould.cs
+++ ScoreKeeper/ScoreKeeper.Core.UnitTests/ScoreKeeperShould.cs
@@ -5,17 +5,42 @@ namespace ScoreKeeper.Core.UnitTests
 {
     public class ScoreKeeperShould
     {
-        [Fact]
-        public void GivenANewScoreKeeper_WhenStartingANewGame_ThenScoreIs0Vs0()
+        [Theory]
+        [InlineData(0, 0, "000:000")]
+        [InlineData(999, 0, "999:000")]
+        [InlineData(0, 999, "000:999")]
+        [InlineData(999, 999, "999:999")]
+        public void GivenANewScoreKeeper_WhenStartingANewGame_ThenScoreIsSet(int initialScoreA, int initialScoreB,
+            string visualScore)
         {
-            var scoreKeeper = new ScoreKeeper();
-            Assert.Equal("000:000", scoreKeeper.GetScore());
+            var scoreKeeper = new ScoreKeeper(initialScoreA, initialScoreB);
+            Assert.Equal(visualScore, scoreKeeper.GetScore());
+        }
+
+        [Theory]
+        [InlineData(1000, 0)]
+        [InlineData(0, 1000)]
+        [InlineData(1000, 1000)]
+        public void GivenANewScoreKeeper_WhenStartingANewGameWithMoreThan999Points_ThenAnExceptionIsThrown(int startingPointA, int startingPointB)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new ScoreKeeper(startingPointA, startingPointB));
+            Assert.Equal(ScoreKeeper.SCORE_IS_INVALID_EXCEPTION, exception.Message);
+        }
+
+        [Theory]
+        [InlineData(-1, 0)]
+        [InlineData(0, -1)]
+        [InlineData(-1, -1)]
+        public void GivenANewScoreKeeper_WhenStartingANewGameWithLessThan1Point_ThenAnExceptionIsThrown(int startingPointA, int startingPointB)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new ScoreKeeper(startingPointA, startingPointB));
+            Assert.Equal(ScoreKeeper.SCORE_IS_INVALID_EXCEPTION, exception.Message);
         }
 
         [Fact]
         public void GivenANewScoreKeeper_WhenTeamAScoresOnePoint_ThenScoreIs1Vs0()
         {
-            var scoreKeeper = new ScoreKeeper();
+            var scoreKeeper = new ScoreKeeper(0, 0);
             scoreKeeper.ScoreTeamA1();
             Assert.Equal("001:000", scoreKeeper.GetScore());
         }
@@ -23,7 +48,7 @@ namespace ScoreKeeper.Core.UnitTests
         [Fact]
         public void GivenANewScoreKeeper_WhenTeamBScoresOnePoint_ThenScoreIs0Vs1()
         {
-            var scoreKeeper = new ScoreKeeper();
+            var scoreKeeper = new ScoreKeeper(0, 0);
             scoreKeeper.ScoreTeamB1();
             Assert.Equal("000:001", scoreKeeper.GetScore());
         }
@@ -31,7 +56,7 @@ namespace ScoreKeeper.Core.UnitTests
         [Fact]
         public void GivenANewScoreKeeper_WhenTeamAScoresTwoPoints_ThenScoreIs2Vs0()
         {
-            var scoreKeeper = new ScoreKeeper();
+            var scoreKeeper = new ScoreKeeper(0, 0);
             scoreKeeper.ScoreTeamA2();
             Assert.Equal("002:000", scoreKeeper.GetScore());
         }
@@ -39,7 +64,7 @@ namespace ScoreKeeper.Core.UnitTests
         [Fact]
         public void GivenANewScoreKeeper_WhenTeamBScoresTwoPoints_ThenScoreIs0Vs2()
         {
-            var scoreKeeper = new ScoreKeeper();
+            var scoreKeeper = new ScoreKeeper(0, 0);
             scoreKeeper.ScoreTeamB2();
             Assert.Equal("000:002", scoreKeeper.GetScore());
         }
@@ -47,7 +72,7 @@ namespace ScoreKeeper.Core.UnitTests
          [Fact]
          public void GivenANewScoreKeeper_WhenTeamAScoresThreePoints_ThenScoreIs3Vs0()
          {
-             var scoreKeeper = new ScoreKeeper();
+             var scoreKeeper = new ScoreKeeper(0, 0);
              scoreKeeper.ScoreTeamA3();
              Assert.Equal("003:000", scoreKeeper.GetScore());
          }
@@ -55,7 +80,7 @@ namespace ScoreKeeper.Core.UnitTests
          [Fact]
          public void GivenANewScoreKeeper_WhenTeamBScoresThreePoints_ThenScoreIs0Vs3()
          {
-             var scoreKeeper = new ScoreKeeper();
+             var scoreKeeper = new ScoreKeeper(0, 0);
              scoreKeeper.ScoreTeamB3();
              Assert.Equal("000:003", scoreKeeper.GetScore());
          }
@@ -63,7 +88,7 @@ namespace ScoreKeeper.Core.UnitTests
          [Fact]
          public void GivenANewScoreKeeper_WhenScoringSeveralTimes_ThenScoreUpdatesCorrectly()
          {
-             var scoreKeeper = new ScoreKeeper();
+             var scoreKeeper = new ScoreKeeper(0, 0);
              scoreKeeper.ScoreTeamA1();
              scoreKeeper.ScoreTeamA3();
              scoreKeeper.ScoreTeamB2();
--- ScoreKeeper/ScoreKeeper.Core/ScoreKeeper.cs
+++ ScoreKeeper/ScoreKeeper.Core/ScoreKeeper.cs
@@ -4,28 +4,50 @@ namespace ScoreKeeper.Core
 {
     public class ScoreKeeper
     {
+        public const string SCORE_IS_INVALID_EXCEPTION = "Score cannot be over 999 points.";
         private int _scoreTeamA = 0;
         private int _scoreTeamB = 0;
 
+        public ScoreKeeper(int startingScoreTeamA, int startingScoreTeamB)
+        {
+            _scoreTeamA = startingScoreTeamA >= 0 && startingScoreTeamA <= 999
+                ? startingScoreTeamA
+                : throw new ArgumentException(SCORE_IS_INVALID_EXCEPTION);
+
+            _scoreTeamB = startingScoreTeamB >= 0 && startingScoreTeamB <= 999
+                ? startingScoreTeamB
+                : throw new ArgumentException(SCORE_IS_INVALID_EXCEPTION);
+        }
+        
         public string GetScore() =>
             $"{_scoreTeamA:D3}:{_scoreTeamB:D3}";
 
         public void ScoreTeamA1() =>
-            _scoreTeamA++;
+            AddToTeamA(1);
 
         public void ScoreTeamB1() =>
-            _scoreTeamB++;
+            AddToTeamB(1);
 
         public void ScoreTeamA2() =>
-            _scoreTeamA += 2;
+            AddToTeamA(2);
 
         public void ScoreTeamB2() =>
-            _scoreTeamB += 2;
+            AddToTeamB(2);
 
         public void ScoreTeamA3() =>
-            _scoreTeamA += 3;
+            AddToTeamA(3);
 
         public void ScoreTeamB3() =>
-            _scoreTeamB += 3;
+            AddToTeamB(3);
+
+        private void AddToTeamA(int scoreToAdd) =>
+            _scoreTeamA = _scoreTeamA + scoreToAdd <= 999
+                ? _scoreTeamA + scoreToAdd
+                : throw new ArgumentOutOfRangeException();
+
+        private void AddToTeamB(int scoreToAdd) =>
+            _scoreTeamB = _scoreTeamB + scoreToAdd <= 999
+                ? _scoreTeamB + scoreToAdd
+                : throw new ArgumentOutOfRangeException();
     }
 }
