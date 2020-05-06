aae580582481d790930c06677a0b8cf4584b5ad9
Roberto <rpgrca@gmail.com>
2020-05-06 10:05:28 -0300

    to expressions

--- ScoreKeeper/ScoreKeeper.Core/ScoreKeeper.cs
+++ ScoreKeeper/ScoreKeeper.Core/ScoreKeeper.cs
@@ -7,39 +7,25 @@ namespace ScoreKeeper.Core
         private int _scoreTeamA = 0;
         private int _scoreTeamB = 0;
 
-        public string GetScore()
-        {
-            return $"{_scoreTeamA:D3}:{_scoreTeamB:D3}";
-        }
+        public string GetScore() =>
+            $"{_scoreTeamA:D3}:{_scoreTeamB:D3}";
 
-        public void ScoreTeamA1()
-        {
+        public void ScoreTeamA1() =>
             _scoreTeamA++;
-        }
 
-        public void ScoreTeamB1()
-        {
+        public void ScoreTeamB1() =>
             _scoreTeamB++;
-        }
 
-        public void ScoreTeamA2()
-        {
+        public void ScoreTeamA2() =>
             _scoreTeamA += 2;
-        }
 
-        public void ScoreTeamB2()
-        {
+        public void ScoreTeamB2() =>
             _scoreTeamB += 2;
-        }
 
-        public void ScoreTeamA3()
-        {
+        public void ScoreTeamA3() =>
             _scoreTeamA += 3;
-        }
 
-        public void ScoreTeamB3()
-        {
+        public void ScoreTeamB3() =>
             _scoreTeamB += 3;
-        }
     }
 }
