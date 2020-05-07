508a20ea743ea34a91142d8d70b71a5b6e5a3524
Roberto <rpgrca@gmail.com>
2020-05-06 22:47:12 -0300

    added numeric and boolean flags

--- CommandLineParser/CommandLineParser.Core.UnitTests/CommandLineParserShould.cs
+++ CommandLineParser/CommandLineParser.Core.UnitTests/CommandLineParserShould.cs
@@ -20,11 +20,56 @@ namespace CommandLineParser.Core.UnitTests
         }
 
         [Fact]
-        public void GivenACommandLineParser_WhenParsingFlagNotInSchema_ThenAnExceptionIsThrown()
+        public void GivenACommandLineParser_WhenParsingFlagWithEmptySchema_ThenAnExceptionIsThrown()
         {
             var commandLineParser = new CommandLineParser(new object());
             var exception = Assert.Throws<ArgumentException>(() => commandLineParser.Parse("-l"));
             Assert.Equal(CommandLineParser.FLAG_IS_UNKNOWN_EXCEPTION, exception.Message);
+        }
+
+        [Fact]
+        public void GivenACommandLineParser_WhenParsingNull_ThenAnExceptionIsThrown()
+        {
+            var commandLineParser = new CommandLineParser(new object());
+            var exception = Assert.Throws<ArgumentException>(() => commandLineParser.Parse(null));
+            Assert.Equal(CommandLineParser.ARGUMENTS_ARE_NULL_EXCEPTION,  exception.Message);
+        }
+
+        [Fact]
+        public void GivenACommandLineParser_WhenParsingBooleanFlagInSchema_ThenTrueIsReturned()
+        {
+            var commandLineParser = new CommandLineParser("l");
+            commandLineParser.Parse("-l");
+            Assert.True(commandLineParser.GetBoolean("l"));
+        }
+
+        [Fact]
+        public void GivenACommandLineParser_WhenBooleanFlagIsNotInArgs_ThenFalseIsReturned()
+        {
+            var commandLineParser = new CommandLineParser("l");
+            commandLineParser.Parse("");
+            Assert.False(commandLineParser.GetBoolean("l"));
+        }
+
+        [Theory]
+        [InlineData("-l 0", 0)]
+        [InlineData("-l 8", 8)]
+        [InlineData("-l 9", 9)]
+        public void GivenACommandLineParser_WhenParsingAFlagWithNumericValue_ThenTheValueIsReturned(string commandLine, int expectedValue)
+        {
+            var commandLineParser = new CommandLineParser("l");
+            commandLineParser.Parse(commandLine);
+            var value = commandLineParser.GetInteger("l");
+            Assert.Equal(expectedValue, value);
+        }
+
+        [Fact]
+        public void GivenACommandLineParser_WhenNumericFlagIsMissingFromArgs_ThenZeroIsReturned()
+        {
+            var commandLineParser = new CommandLineParser("l");
+            commandLineParser.Parse("");
+            var value = commandLineParser.GetInteger("l");
+            Assert.Equal(0, value);
         }
     }
 }
--- CommandLineParser/CommandLineParser.Core/CommandLineParser.cs
+++ CommandLineParser/CommandLineParser.Core/CommandLineParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CommandLineParser.Core
 {
@@ -6,14 +7,41 @@ namespace CommandLineParser.Core
     {
         public const string FLAG_IS_UNKNOWN_EXCEPTION = "The given flag is invalid.";
         public const string SCHEMA_IS_NULL_EXCEPTION = "Schema is null.";
+        public const string ARGUMENTS_ARE_NULL_EXCEPTION = "Argument line is null.";
+        
+        private readonly object _parserSchema;
+        private string _commandLine;
+
         public CommandLineParser(object parserSchema)
         {
-            _ = parserSchema ?? throw new ArgumentException(SCHEMA_IS_NULL_EXCEPTION);
+            _parserSchema = parserSchema ?? throw new ArgumentException(SCHEMA_IS_NULL_EXCEPTION);
         }
 
         public void Parse(string commandLine)
         {
-            throw new ArgumentException(FLAG_IS_UNKNOWN_EXCEPTION);
+            _commandLine = commandLine ?? throw new ArgumentException(ARGUMENTS_ARE_NULL_EXCEPTION);
+            if (string.IsNullOrEmpty(_commandLine))
+                return;
+
+            if (! _commandLine.StartsWith($"-{_parserSchema}"))
+            {
+                throw new ArgumentException(FLAG_IS_UNKNOWN_EXCEPTION);
+            }
+        }
+
+        public bool GetBoolean(string flag)
+        {
+            return ($"-{_parserSchema}" == _commandLine);
+        }
+
+        public int GetInteger(string flag)
+        {
+            if (_commandLine.StartsWith($"-{_parserSchema}"))
+            {
+                return int.Parse(_commandLine.Substring(_parserSchema.ToString().Length + 1));
+            }
+            
+            return 0;
         }
     }
 }
