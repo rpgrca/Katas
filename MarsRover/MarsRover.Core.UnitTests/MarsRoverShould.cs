81c7d81e510f949c0ebc4461623a8bb983995e5b
Roberto <rpgrca@gmail.com>
2020-05-11 19:10:44 -0300

    initial upload

---
+++ MarsRover/MarsRover.Core.UnitTests/MarsRoverShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MarsRover.Core.UnitTests
+{
+    public class MarsRoverShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ MarsRover/MarsRover.Core/MarsRover.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MarsRover.Core
+{
+    public class MarsRover
+    {
+    }
+}
