e0f6a91069027d611b7ebc94c2cc420b4480d871
Roberto <rpgrca@gmail.com>
2020-05-11 20:00:32 -0300

    developing some functionality

--- MarsRover/MarsRover.Core.UnitTests/MarsRoverShould.cs
+++ MarsRover/MarsRover.Core.UnitTests/MarsRoverShould.cs
@@ -8,7 +8,99 @@ namespace MarsRover.Core.UnitTests
         [Fact]
         public void Test1()
         {
+            var marsRover = new MarsRover(1, 1, "N");
+            marsRover.Process("");
+            Assert.True(marsRover.IsAtHeadingTo(1, 1, "N"));
+        }
 
+        [Fact]
+        public void Test2()
+        {
+            var marsRover = new MarsRover(1, 1, "N");
+
+            marsRover.Process("f");
+
+            Assert.False(marsRover.IsAtHeadingTo(1, 1, "N"));
+            Assert.True(marsRover.IsAtHeadingTo(1, 2, "N"));
+        }
+
+        [Fact]
+        public void Test3(){
+            var marsRover = new MarsRover(1,1,"N");
+ 
+            marsRover.Process("ff");
+ 
+            Assert.True(marsRover.IsAtHeadingTo(1,3,"N"));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var marsRover = new MarsRover(1, 1, "N");
+
+            marsRover.Process("b");
+
+            Assert.True(marsRover.IsAtHeadingTo(1, 0, "N"));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var marsRover = new MarsRover(1, 1, "N");
+
+            marsRover.Process("l");
+
+            Assert.False(marsRover.IsAtHeadingTo(1, 1, "N"));
+            Assert.True(marsRover.IsAtHeadingTo(1, 1, "W"));
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            var marsRover = new MarsRover(1, 1, "N");
+
+            marsRover.Process("r");
+
+            Assert.False(marsRover.IsAtHeadingTo(1, 1, "N"));
+            Assert.True(marsRover.IsAtHeadingTo(1, 1, "E"));
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            var marsRover = new MarsRover(1, 1, "N");
+
+            var exception = Assert.Throws<ArgumentException>(() => marsRover.Process("x"));
+            Assert.Equal(MarsRover.INVALID_COMMAND, exception.Message);
+        }
+
+        [Fact]
+        public void Test71()
+        {
+            var marsRover = new MarsRover(1, 1, "N");
+
+            Assert.Throws<ArgumentException>(() => marsRover.Process("x"));
+            Assert.True(marsRover.IsAtHeadingTo(1, 1, "N"));
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            var marsRover = new MarsRover(1, 1, "E");
+
+            marsRover.Process("f");
+
+            Assert.True(marsRover.IsAtHeadingTo(2, 1, "E"));
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            var marsRover = new MarsRover(1, 1, "E");
+
+            marsRover.Process("br");
+
+            Assert.True(marsRover.IsAtHeadingTo(0, 1, "S"));
         }
     }
 }
--- MarsRover/MarsRover.Core/MarsRover.cs
+++ MarsRover/MarsRover.Core/MarsRover.cs
@@ -4,5 +4,72 @@ namespace MarsRover.Core
 {
     public class MarsRover
     {
+        private int _x;
+        private int _y;
+        private string _headingTo;
+        public const string INVALID_COMMAND = "Invalid command";
+
+        public MarsRover(int x, int y, string headingTo)
+        {
+            _x = x;
+            _y = y;
+            _headingTo = headingTo;
+        }
+
+        public void Process(string commands)
+        {
+            foreach (var command in commands.ToCharArray())
+            {
+                if (command == 'f')
+                {
+                    if (_headingTo == "N")
+                    {
+                        _y++;
+                    }
+                    else if (_headingTo == "E")
+                    {
+                        _x++;
+                    }
+                }
+                else if (command == 'b')
+                {
+                    if (_headingTo == "N")
+                    {
+                        _y--;
+                    }
+                    else if (_headingTo == "E")
+                    {
+                        _x--;
+                    }
+                }
+                else if (command == 'l')
+                {
+                    if (_headingTo == "N")
+                    {
+                        _headingTo = "W";
+                    }
+                }
+                else if (command == 'r')
+                {
+                    if (_headingTo == "N")
+                    {
+                        _headingTo = "E";
+                    }
+                    else if (_headingTo == "E")
+                    {
+                        _headingTo = "S";
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException(INVALID_COMMAND);
+                }
+            }
+        }
+
+        public bool IsAtHeadingTo(int x, int y, string headingTo)
+        {
+            return _x == x && _y == y && _headingTo == headingTo;
+        }
     }
 }
