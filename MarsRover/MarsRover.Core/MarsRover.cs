2579117d83d194af1d6bfe4659049e256a36fb5b
Roberto <rpgrca@gmail.com>
2020-05-12 00:13:35 -0300

    c# 8, solution with state machine

--- MarsRover/MarsRover.Core/MarsRover.cs
+++ MarsRover/MarsRover.Core/MarsRover.cs
@@ -1,78 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MarsRover.Core
 {
     public class MarsRover
     {
+        public const string INVALID_COMMAND = "Invalid command";
+
         private int _x;
         private int _y;
-        private string _headingTo;
-        public const string INVALID_COMMAND = "Invalid command";
+        private int _headingTo;
+        private readonly string[] _directions = { "N", "E", "S", "W"};
+        private readonly List<Dictionary<char, (int, int)>> _movements = new List<Dictionary<char, (int, int)>>
+        {
+            { new Dictionary<char, (int, int)> { { 'f', (0, 1) }, { 'b', (0, -1) } } },
+            { new Dictionary<char, (int, int)> { { 'f', (1, 0) }, { 'b', (-1, 0) } } },
+            { new Dictionary<char, (int, int)> { { 'f', (0, -1) }, { 'b', (0, 1) } } },
+            { new Dictionary<char, (int, int)> { { 'f', (-1, 0) }, { 'b', (1, 0) } } }
+        };
 
         public MarsRover(int x, int y, string headingTo)
         {
             _x = x;
             _y = y;
-            _headingTo = headingTo;
+            _headingTo = Array.IndexOf(_directions, headingTo);
         }
+
+        private void TurnAround(char command) =>
+            _headingTo = (_headingTo + (command == 'r' ? 1 : 3)) % 4;
+
+        private (int, int) GetMovementOffset(char command) =>
+            _movements[_headingTo][command];
 
         public void Process(string commands)
         {
-            foreach (var command in commands.ToCharArray())
+            foreach (var command in commands)
             {
-                if (_headingTo == "N")
+                switch (command)
                 {
-                    if (command == 'f')
-                    {
-                        _y++;
-                    }
-                    else if (command == 'b')
-                    {
-                        _y--;
-                    }
-                    else if (command == 'l')
-                    {
-                        _headingTo = "W";
-                    }
-                    else if (command == 'r')
-                    {
-                        _headingTo = "E";
-                    }
-                    else
-                    {
+                    case 'l':
+                    case 'r':
+                        TurnAround(command);
+                        break;
+                    case 'f':
+                    case 'b':
+                        var (offsetX, offsetY) = GetMovementOffset(command);
+                        _x += offsetX;
+                        _y += offsetY;
+                        break;
+                    default:
                         throw new ArgumentException(INVALID_COMMAND);
-                    }
-                }
-
-                else if (_headingTo == "E")
-                {
-                    if (command == 'f')
-                    {
-                        _x++;
-                    }
-                    else if (command == 'b')
-                    {
-                        _x--;
-                    }
-                    else if (command == 'l')
-                    {
-                            _headingTo = "N";
-                    }
-                    else if (command == 'r')
-                    {
-                        _headingTo = "S";
-                    }
-                    else
-                    {
-                        throw new ArgumentException(INVALID_COMMAND);
-                    }
                 }
             }
         }
 
-        public bool IsAtHeadingTo(int x, int y, string headingTo)
-        {
-            return _x == x && _y == y && _headingTo == headingTo;
-        }
+        public bool IsAtHeadingTo(int x, int y, string headingTo) =>
+            _x == x && _y == y && _directions[_headingTo] == headingTo;
     }
 }
