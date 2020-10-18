e3d49960d4581f5dfdbefbb7a9862ebcce7dca60
Roberto <rpgrca@gmail.com>
2020-10-18 00:33:49 -0300

    add classes for multiples of 3, 5, and 3 and 5

--- FizzBuzz/FizzBuzz.OO/Number.cs
+++ FizzBuzz/FizzBuzz.OO/Number.cs
@@ -5,9 +5,36 @@ using System;
 
 namespace FizzBuzz.OO
 {
+    public class MultipleOfThree : Number
+    {
+        public MultipleOfThree(int value) : base(value)
+        {
+        }
+
+        public override string ToString() => "Fizz";
+    }
+
+    public class MultipleOfFive : Number
+    {
+        public MultipleOfFive(int value) : base(value)
+        {
+        }
+
+        public override string ToString() => "Buzz";
+    }
+
+    public class MultipleOfThreeAndFive : Number
+    {
+        public MultipleOfThreeAndFive(int value) : base(value)
+        {
+        }
+
+        public override string ToString() => "FizzBuzz";
+    }
+
     public class Number : Element
     {
-        private readonly int _value;
+        protected readonly int _value;
 
         public Number(int value) => _value = value;
 
