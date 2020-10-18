98b1c1824bb964df14d69c241dad70f3dfc7a9c7
Roberto <rpgrca@gmail.com>
2020-10-18 00:11:21 -0300

    implement minimum number, add nubmer list

--- FizzBuzz/FizzBuzz.OO/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.OO/FizzBuzz.cs
@@ -1,23 +1,27 @@
 ﻿using System.Globalization;
+using System.Collections.Generic;
 using System;
 
 namespace FizzBuzz.OO
 {
-    public abstract class Element
+    public interface Element
     {
-        public abstract string ToString();
+        string ToString();
     }
 
     public class Number : Element
     {
-        public override string ToString()
-        {
-        }
+        private readonly int _value;
+
+        public Number(int value) => _value = value;
+
+        public string ToString() => _value.ToString();
     }
 
     public class FizzBuzz
     {
         private readonly int _limit;
+        private List<Element> _numbers;
 
         public FizzBuzz(int limit)
         {
