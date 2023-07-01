0d433e3d5afc71d2c07709e5dd8736fccd1ee3ec
Roberto <rpgrca@gmail.com>
2023-07-01 19:30:14 -0300

    add test, item sell-in decreases when updating

--- InventoryManager/InventoryManager.UnitTests/UpdateQualityMust.cs
+++ InventoryManager/InventoryManager.UnitTests/UpdateQualityMust.cs
@@ -22,4 +22,23 @@ public class UpdateQualityMust
         Assert.Equal("Anything", item.Name);
         Assert.Equal(9, item.Quality);
     }
+
+    [Fact]
+    public void DecreaseSellInTime_WhenItIsNotSpecial()
+    {
+        var item = new Item
+        {
+            Name = "Anything",
+            SellIn = 10,
+            Quality = 10
+        };
+
+        var items = new[] { item };
+
+        var sut = new Logic.InventoryManager();
+        sut.UpdateQuality(items);
+
+        Assert.Equal("Anything", item.Name);
+        Assert.Equal(9, item.SellIn);
+    }
 }
