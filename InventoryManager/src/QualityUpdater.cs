65e131e3a15d2b37f340b3445060e74c11dba06f
Roberto <rpgrca@gmail.com>
2023-07-01 22:31:07 -0300

    extract rule for Aged Brie quality

--- InventoryManager/src/QualityUpdater.cs
+++ InventoryManager/src/QualityUpdater.cs
@@ -1,13 +1,50 @@
 namespace InventoryManager.Logic;
 
+public class Rule
+{
+    private readonly string _name;
+    private readonly Func<Item, bool> _qualityUpdateCondition;
+    private readonly Action<Item> _qualityUpdate;
+
+    public Rule(string name, Func<Item, bool> qualityUpdateCondition, Action<Item> qualityUpdate)
+    {
+        _name = name;
+        _qualityUpdateCondition = qualityUpdateCondition;
+        _qualityUpdate = qualityUpdate;
+    }
+
+    public bool CanApplyOn(Item item) => _name == item.Name || string.IsNullOrEmpty(_name);
+
+    public void UpdateItem(Item item) {
+        if (_qualityUpdateCondition(item))
+        {
+            _qualityUpdate(item);
+        }
+    }
+}
+
 public class QualityUpdater
 {
     private readonly Item _item;
+    private readonly Rule[] _rules;
 
-    public QualityUpdater(Item item) => _item = item;
+    public QualityUpdater(Item item)
+    {
+        _item = item;
+        _rules = new Rule[]
+        {
+            new("Sulfuras, Hand of Ragnaros", i => false, i => {}),
+            new("Aged Brie", i => i.Quality < 50, i => i.Quality += 1),
+            new("Backstage passes to a TAFKAL80ETC concert", i => false, i => {}),
+            new("", i => false, i => {})
+        };
+    }
 
     public void Update()
     {
+        var rule = _rules.First(r => r.CanApplyOn(_item));
+        rule?.UpdateItem(_item);
+
         if (_item.Name == "Sulfuras, Hand of Ragnaros")
         {
             return;
@@ -18,14 +55,6 @@ public class QualityUpdater
             if (CanDecreaseQuality())
             {
                 DecreaseQuality();
-            }
-        }
-
-        if (_item.Name == "Aged Brie")
-        {
-            if (CanIncrementQuality())
-            {
-                IncreaseQuality();
             }
         }
 
