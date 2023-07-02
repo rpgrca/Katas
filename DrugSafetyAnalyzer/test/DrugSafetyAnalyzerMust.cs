cf6a771c38254dd395f2c4d2e4714818c972f225
Roberto <rpgrca@gmail.com>
2023-07-02 10:41:19 -0300

    add tests for checking safe levels

--- DrugSafetyAnalyzer/test/DrugSafetyAnalyzerMust.cs
+++ DrugSafetyAnalyzer/test/DrugSafetyAnalyzerMust.cs
@@ -4,18 +4,70 @@ namespace DrugSafetyAnalyzer.UnitTests;
 
 public class DrugSafetyAnalyzerMust
 {
-    [Fact]
-    public void ReturnSafeLevel_WhenDrugHasOnlyAspirinAsIngredient()
+    [Theory]
+    [InlineData("Caffeine")]
+    [InlineData("Aspirin")]
+    [InlineData("Paracetamol")]
+    [InlineData("Ibuprofen")]
+    [InlineData("Acetaminophen")]
+    [InlineData("Codeine")]
+    [InlineData("Warfarin")]
+    public void ReturnSafeLevel_WhenDrugHasOnlyIngredient(string ingredient)
     {
         var drug = new Drug
         {
-            Name = "Bayaspirina",
-            Ingredients = new() { "Aspirin" }
+            Name = "Safe Drug",
+            Ingredients = new() { ingredient }
         };
 
         var sut = new Logic.DrugSafetyAnalyzer();
         var result = sut.AnalyzeSafetyLevel(drug);
 
         Assert.Equal(SafetyLevel.Safe, result);
+    }
+
+    [Fact]
+    public void ReturnDangerousLevel_WhenDrugHasParacetamolAndIbuprofen()
+    {
+        var drug = new Drug
+        {
+            Name = "Dangerous Drug",
+            Ingredients = new() { "Paracetamol", "Ibuprofen" }
+        };
+
+        var sut = new Logic.DrugSafetyAnalyzer();
+        var result = sut.AnalyzeSafetyLevel(drug);
+
+        Assert.Equal(SafetyLevel.Dangerous, result);
+    }
+
+    [Fact]
+    public void ReturnDangerousLevel_WhenDrugHasAcetaminophenAndCodeine()
+    {
+        var drug = new Drug
+        {
+            Name = "Dangerous Drug",
+            Ingredients = new() { "Acetaminophen", "Codeine" }
+        };
+
+        var sut = new Logic.DrugSafetyAnalyzer();
+        var result = sut.AnalyzeSafetyLevel(drug);
+
+        Assert.Equal(SafetyLevel.Dangerous, result);
+    }
+
+    [Fact]
+    public void ReturnMortalLevel_WhenDrugHasAspirinAndWarfarin()
+    {
+        var drug = new Drug
+        {
+            Name = "Mortal Drug",
+            Ingredients = new() { "Aspirin", "Warfarin" }
+        };
+
+        var sut = new Logic.DrugSafetyAnalyzer();
+        var result = sut.AnalyzeSafetyLevel(drug);
+
+        Assert.Equal(SafetyLevel.Mortal, result);
     }
 }
