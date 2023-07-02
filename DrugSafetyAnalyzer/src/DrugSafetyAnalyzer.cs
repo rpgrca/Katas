173cbaa990870ffec96b325499c762f9a671d4f6
Roberto <rpgrca@gmail.com>
2023-07-02 19:49:11 -0300

    refactor with rules

--- DrugSafetyAnalyzer/src/DrugSafetyAnalyzer.cs
+++ DrugSafetyAnalyzer/src/DrugSafetyAnalyzer.cs
@@ -1,26 +1,40 @@
 ﻿namespace DrugSafetyAnalyzer.Logic;
 
+public record Rule(string[] Ingredients, SafetyLevel Level);
+
 public class DrugSafetyAnalyzer
 {
+    private readonly List<Rule> _rules;
+    private readonly Rule _defaultSafeRule;
+
+    public DrugSafetyAnalyzer()
+    {
+        _rules = new List<Rule>
+        {
+            { new(new[] { "Acetaminophen", "Codeine" }, SafetyLevel.Dangerous) },
+            { new(new[] { "Aspirin", "Warfarin" }, SafetyLevel.Mortal) },
+            { new(new[] { "Ibuprofen", "Paracetamol" }, SafetyLevel.Dangerous) }
+        };
+
+        _defaultSafeRule = new(Array.Empty<string>(), SafetyLevel.Safe);
+    }
+
     public SafetyLevel AnalyzeSafetyLevel(Drug drug)
     {
-        var ingredients = drug.Ingredients;
+        var rule = FindFittingtRuleFor(drug);
+        return rule.Level;
+    }
 
-        if (ingredients.Contains("Paracetamol") && ingredients.Contains("Ibuprofen"))
+    private Rule FindFittingtRuleFor(Drug drug)
+    {
+        foreach (var rule in _rules)
         {
-            return SafetyLevel.Dangerous;
+            if (! rule.Ingredients.Except(drug.Ingredients).Any())
+            {
+                return rule;
+            }
         }
-        else if (ingredients.Contains("Acetaminophen") && ingredients.Contains("Codeine"))
-        {
-            return SafetyLevel.Dangerous;
-        }
-        else if (ingredients.Contains("Aspirin") && ingredients.Contains("Warfarin"))
-        {
-            return SafetyLevel.Mortal;
-        }
-        else
-        {
-            return SafetyLevel.Safe;
-        }
+
+        return _defaultSafeRule;
     }
 }
