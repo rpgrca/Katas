5272b97c06ca328aa49e467106977c014f39a357
Roberto <rpgrca@gmail.com>
2023-07-02 10:30:39 -0300

    add test, recognize safe drugs

--- DrugSafetyAnalyzer/test/DrugSafetyAnalyzerMust.cs
+++ DrugSafetyAnalyzer/test/DrugSafetyAnalyzerMust.cs
@@ -1,10 +1,21 @@
+using DrugSafetyAnalyzer.Logic;
+
 namespace DrugSafetyAnalyzer.UnitTests;
 
 public class DrugSafetyAnalyzerMust
 {
     [Fact]
-    public void Test1()
+    public void ReturnSafeLevel_WhenDrugHasOnlyAspirinAsIngredient()
     {
+        var drug = new Drug
+        {
+            Name = "Bayaspirina",
+            Ingredients = new() { "Aspirin" }
+        };
 
+        var sut = new Logic.DrugSafetyAnalyzer();
+        var result = sut.AnalyzeSafetyLevel(drug);
+
+        Assert.Equal(SafetyLevel.Safe, result);
     }
 }
