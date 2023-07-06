cd008f4aca8a36190c7555915bc449bd713074dc
Roberto <rpgrca@gmail.com>
2023-07-05 22:10:39 -0300

    add lcov.info to main gitignore, remove others

--- ProductPricing/test/PricingCalculatorMust.cs
+++ ProductPricing/test/PricingCalculatorMust.cs
@@ -1,10 +1,23 @@
+using ProductPricing.Logic;
+
 namespace ProductPricing.UnitTests;
 
-public class UnitTest1
+public class PricingCalculatorMust
 {
     [Fact]
-    public void Test1()
+    public void ReturnSamePrice_WhenCategoryIsUnknownAndIsNotTaxableNorImported()
     {
+        var product = new Product
+        {
+            Name = "Shirt",
+            BasePrice = 100,
+            Category = "Unknown",
+            IsTaxable = false,
+            IsImported = false
+        };
+        var sut = new PricingCalculator();
 
+        var result = sut.CalculatePrice(product);
+        Assert.Equal(100, result);
     }
 }
