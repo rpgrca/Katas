1ba861feac51f584bdf0afe0f827b56f05caf596
Roberto <rpgrca@gmail.com>
2023-07-05 23:38:06 -0300

    add test, product with unknown category, imported and taxed

--- ProductPricing/test/PricingCalculatorWithUnknownInventoryMust.cs
+++ ProductPricing/test/PricingCalculatorWithUnknownInventoryMust.cs
@@ -7,6 +7,7 @@ public class PricingCalculatorWithUnknownCategoryMust
     private const decimal Price = 100;
     private const decimal PriceWithImportDuties = 105;
     private const decimal PriceWithTax = 110;
+    private const decimal PriceWithTaxAndImportDuties = 115.5M;
 
     [Fact]
     public void ReturnSamePrice_WhenIsNotTaxableNorImported()
@@ -47,5 +48,16 @@ public class PricingCalculatorWithUnknownCategoryMust
 
         var result = sut.CalculatePrice(product);
         Assert.Equal(PriceWithTax, result);
+    }
+
+    [Fact]
+    public void ReturnCorrectPrice_WhenProductHasTaxAndIsImported()
+    {
+        var product = CreateProductWithUnknownCategory(taxable: true, imported: true);
+        var sut = new PricingCalculator();
+
+        var result = sut.CalculatePrice(product);
+        Assert.Equal(PriceWithTaxAndImportDuties, result);
+
     }
 }
