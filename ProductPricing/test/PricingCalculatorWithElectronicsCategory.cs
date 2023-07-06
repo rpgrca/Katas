cd7188e20b50bfde3117d51173d31b8212070a71
Roberto <rpgrca@gmail.com>
2023-07-05 23:07:20 -0300

    add test, make small discount in cheap Electronics

--- ProductPricing/test/PricingCalculatorWithElectronicsCategory.cs
+++ ProductPricing/test/PricingCalculatorWithElectronicsCategory.cs
@@ -4,4 +4,27 @@ namespace ProductPricing.UnitTests;
 
 public class PricingCalculatorWithElectronicsCategoryMust
 {
+    private const decimal CheapPrice = 100;
+    private const decimal CheapPriceWithSmallDiscount = 80;
+ 
+    [Fact]
+    public void ReturnSamePrice_WhenIsNotTaxableNorImported()
+    {
+        var product = CreateProductWithElectronicsCategory();
+        var sut = new PricingCalculator();
+
+        var result = sut.CalculatePrice(product);
+        Assert.Equal(CheapPriceWithSmallDiscount, result);
+    }
+
+    private static Product CreateProductWithElectronicsCategory(decimal price = CheapPrice,
+        bool taxable = false, bool imported = false) =>
+        new()
+        {
+            Name = "Shirt",
+            BasePrice = price,
+            Category = "Electronics",
+            IsTaxable = taxable,
+            IsImported = imported
+        };
 }
