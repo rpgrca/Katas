c7034c3ad7ee30bcceaee6ac820ab79287a260d3
Roberto <rpgrca@gmail.com>
2023-07-05 22:57:04 -0300

    add test, return correct price with unknown category only

---
+++ ProductPricing/test/PricingCalculatorWithUnknownInventoryMust.cs
@@ -0,0 +1,32 @@
+using ProductPricing.Logic;
+
+namespace ProductPricing.UnitTests;
+
+public class PricingCalculatorWithUnknownInventoryMust
+{
+    private const decimal Price = 100;
+    private const decimal PriceWithImportDuties = 105;
+    private const decimal PriceWithTax = 110;
+
+    [Fact]
+    public void ReturnSamePrice_WhenIsNotTaxableNorImported()
+    {
+        var product = CreateProductWithUnknownCategory();
+        var sut = new PricingCalculator();
+
+        var result = sut.CalculatePrice(product);
+        Assert.Equal(Price, result);
+    }
+
+    private static Product CreateProductWithUnknownCategory(decimal price = Price,
+        bool taxable = false, bool imported = false) =>
+        new()
+        {
+            Name = "Shirt",
+            BasePrice = price,
+            Category = "Unknown",
+            IsTaxable = taxable,
+            IsImported = imported
+        };
+
+}
