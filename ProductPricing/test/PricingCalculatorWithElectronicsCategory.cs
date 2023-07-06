74a2651bf593a9678b6f0d1d526b394261419e3c
Roberto <rpgrca@gmail.com>
2023-07-05 23:56:32 -0300

    add test, correct price for imported electronics with tax

--- ProductPricing/test/PricingCalculatorWithElectronicsCategory.cs
+++ ProductPricing/test/PricingCalculatorWithElectronicsCategory.cs
@@ -8,15 +8,17 @@ public class PricingCalculatorWithElectronicsCategoryMust
     private const int CheapPriceWithSmallDiscount = 80;
     private const int CheapPriceWithSmallDiscountWithTax = 88;
     private const int CheapPriceWithSmallDiscountImported = 84;
+    private const double CheapPriceWithSmallDiscountImportedAndTaxed = 92.4;
     private const int ExpensivePrice = 10000;
     private const int ExpensivePriceWithLargeDiscount = 9950;
     private const double ExpensivePriceWithLargeDiscountWithTax = 10945;
     private const double ExpensivePriceWithLargeDiscountImported = 10447.5;
+    private const double ExpensivePriceWithLargeDiscountImportedAndTaxed = 11492.25;
  
     [Theory]
     [InlineData(CheapPrice, CheapPriceWithSmallDiscount)]
     [InlineData(ExpensivePrice, ExpensivePriceWithLargeDiscount)]
-    public void ReturnSamePrice_WhenIsNotTaxableNorImported(decimal price, decimal expectedTotal)
+    public void ReturnCorrectPrice_WhenIsNotTaxableNorImported(decimal price, decimal expectedTotal)
     {
         var product = CreateProductWithElectronicsCategory(price: price);
         var sut = new PricingCalculator();
@@ -39,7 +41,7 @@ public class PricingCalculatorWithElectronicsCategoryMust
     [Theory]
     [InlineData(CheapPrice, CheapPriceWithSmallDiscountWithTax)]
     [InlineData(ExpensivePrice, ExpensivePriceWithLargeDiscountWithTax)]
-    public void ReturnSamePrice_WhenIsTaxedAndNotImported(decimal price, decimal expectedTotal)
+    public void ReturnCorrectPrice_WhenIsTaxedAndNotImported(decimal price, decimal expectedTotal)
     {
         var product = CreateProductWithElectronicsCategory(price: price, taxable: true);
         var sut = new PricingCalculator();
@@ -51,9 +53,21 @@ public class PricingCalculatorWithElectronicsCategoryMust
     [Theory]
     [InlineData(CheapPrice, CheapPriceWithSmallDiscountImported)]
     [InlineData(ExpensivePrice, ExpensivePriceWithLargeDiscountImported)]
-    public void ReturnSamePrice_WhenIsImportedAndNotTaxed(decimal price, decimal expectedTotal)
+    public void ReturnCorrectPrice_WhenIsImportedAndNotTaxed(decimal price, decimal expectedTotal)
     {
         var product = CreateProductWithElectronicsCategory(price: price, imported: true);
+        var sut = new PricingCalculator();
+
+        var result = sut.CalculatePrice(product);
+        Assert.Equal(expectedTotal, result);
+    }
+
+    [Theory]
+    [InlineData(CheapPrice, CheapPriceWithSmallDiscountImportedAndTaxed)]
+    [InlineData(ExpensivePrice, ExpensivePriceWithLargeDiscountImportedAndTaxed)]
+    public void ReturnCorrectPrice_WhenIsImportedAndTaxed(decimal price, decimal expectedTotal)
+    {
+        var product = CreateProductWithElectronicsCategory(price: price, taxable: true, imported: true);
         var sut = new PricingCalculator();
 
         var result = sut.CalculatePrice(product);
