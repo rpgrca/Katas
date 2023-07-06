79a621b5e495590bea3f7aec4fb1c00e5e10776c
Roberto <rpgrca@gmail.com>
2023-07-06 00:12:28 -0300

    add test, calculate for imported and taxable furniture

--- ProductPricing/test/PricingCalculatorWithFurnitureCategory.cs
+++ ProductPricing/test/PricingCalculatorWithFurnitureCategory.cs
@@ -8,6 +8,7 @@ public class PricingCalculatorWithFurnitureCategoryMust
     public const decimal Total = 1100;
     public const decimal TotalWithImportDuties = 1155;
     public const decimal TotalWithTaxes = 1210;
+    public const decimal TotalWithImportDutiesAndTaxes = 1270.5M;
 
     [Fact]
     public void ReturnCorrectPrice_WhenIsNotTaxableNorImported()
@@ -47,5 +48,15 @@ public class PricingCalculatorWithFurnitureCategoryMust
 
         var result = sut.CalculatePrice(product);
         Assert.Equal(TotalWithTaxes, result);
+    }
+
+    [Fact]
+    public void ReturnCorrectPrice_WhenIsImportedAndTaxed()
+    {
+        var product = CreateProductWithFurnitureCategory(taxable: true, imported: true);
+        var sut = new PricingCalculator();
+
+        var result = sut.CalculatePrice(product);
+        Assert.Equal(TotalWithImportDutiesAndTaxes, result);
     }
 }
