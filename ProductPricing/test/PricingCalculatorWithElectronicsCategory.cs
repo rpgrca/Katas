f4cfd392616185de13abac8a78e379834f7e25f3
Roberto <rpgrca@gmail.com>
2023-07-05 23:33:50 -0300

    add test, return correct price when Electronics is taxed

--- ProductPricing/test/PricingCalculatorWithElectronicsCategory.cs
+++ ProductPricing/test/PricingCalculatorWithElectronicsCategory.cs
@@ -6,8 +6,10 @@ public class PricingCalculatorWithElectronicsCategoryMust
 {
     private const int CheapPrice = 100;
     private const int CheapPriceWithSmallDiscount = 80;
+    private const int CheapPriceWithSmallDiscountWithTax = 88;
     private const int ExpensivePrice = 10000;
     private const int ExpensivePriceWithLargeDiscount = 9950;
+    private const double ExpensivePriceWithLargeDiscountWithTax = 10945;
  
     [Theory]
     [InlineData(CheapPrice, CheapPriceWithSmallDiscount)]
@@ -31,4 +33,18 @@ public class PricingCalculatorWithElectronicsCategoryMust
             IsTaxable = taxable,
             IsImported = imported
         };
+
+    [Theory]
+    [InlineData(CheapPrice, CheapPriceWithSmallDiscountWithTax)]
+    [InlineData(ExpensivePrice, ExpensivePriceWithLargeDiscountWithTax)]
+    public void ReturnSamePrice_WhenIsTaxedAndNotImported(decimal price, decimal expectedTotal)
+    {
+        var product = CreateProductWithElectronicsCategory(price: price, taxable: true);
+        var sut = new PricingCalculator();
+
+        var result = sut.CalculatePrice(product);
+        Assert.Equal(expectedTotal, result);
+    }
+
+
 }
